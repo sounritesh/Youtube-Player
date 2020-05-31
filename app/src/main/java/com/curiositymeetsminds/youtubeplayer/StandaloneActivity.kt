120b04125b28b88c689b42a50bd80a94813b1ea9
sounritesh <sounritesh@gmail.com>
2020-05-31 22:46:04 +0530

    More error handling added

--- app/src/main/java/com/curiositymeetsminds/youtubeplayer/StandaloneActivity.kt
+++ app/src/main/java/com/curiositymeetsminds/youtubeplayer/StandaloneActivity.kt
@@ -1,13 +1,17 @@
 package com.curiositymeetsminds.youtubeplayer
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.youtube.player.YouTubeStandalonePlayer
 import kotlinx.android.synthetic.main.activity_standalone.*
+import java.lang.Exception
 import java.lang.IllegalArgumentException
 
 class StandaloneActivity: AppCompatActivity(), View.OnClickListener {
+    private val tag = "StandaloneActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,12 @@ class StandaloneActivity: AppCompatActivity(), View.OnClickListener {
             R.id.btnPlaylist -> YouTubeStandalonePlayer.createPlaylistIntent(this, getString(R.string.GOOGLE_API_KEY), PLAYLIST_ID, 0, 0, true, true)
             else -> throw IllegalArgumentException("Unassigned button was clicked")
         }
-        startActivity(intent)
+        try {
+            startActivity(intent)
+        } catch (e: Exception) {
+            Log.e(tag, "Error occurred: ${e.message}")
+            Toast.makeText(this, "Error Occurred: ${e.message}", Toast.LENGTH_LONG).show()
+        }
     }
+
 }
